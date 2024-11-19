e88e5e9bcc332cf86fc4b1334d3026d23d1f8881
SIVAKUMAR18MCA <sivakumar18mca@gmail.com>
2024-11-19 16:33:53 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -9,20 +9,20 @@ if [ $GIT_BRANCH = "origin/dev" ]; then
     ./build.sh
 
     # Tag the Docker image for the dev environment
-    docker tag reactapp $DOCKER_USERNAME/dev
+    docker tag sivaapp:reactapp $DOCKER_USERNAME/dev:reactapp
 
     # Push the Docker image to the dev repository
-    docker push $DOCKER_USERNAME/dev
+    docker push $DOCKER_USERNAME/dev:reactapp
 
 elif [$GIT_BRANCH = "origin/main"]; then
     chmod +x build.sh
     ./build.sh
 
     # Tag the Docker image for the prod environment
-    docker tag reactapp $DOCKER_USERNAME/prod
+    docker tag sivaapp:reactapp $DOCKER_USERNAME/prod:reactapp
 
     # Push the Docker image to the prod repository
-    docker push $DOCKER_USERNAME/prod
+    docker push $DOCKER_USERNAME/prod:reactapp
 else 
     echo "Branch-not recognized. No build or push will occur."
     exit 1
